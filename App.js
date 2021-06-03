b793be5983d094a8c2885cca85f8d9bba33bc673
unknown <vettesegiovanni@gmail.com>
2021-06-03 15:40:05 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import Navigation from "./navigation";
+import { Provider } from "react-redux";
+import { store,persistor } from "./src/Redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    return (
+      <Provider store={store}>
+				<PersistGate loading={null} persistor={persistor}>
+                    <Navigation/>
+				</PersistGate>
+		  </Provider>
+    );
+} 
