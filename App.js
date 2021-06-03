078e44613c16c2988ffb70abcf5bf1df79ad7aea
unknown <vettesegiovanni@gmail.com>
2021-06-03 17:11:36 +0200

    first commit

--- App.js
+++ App.js
@@ -1,15 +1,11 @@
 import * as React from "react";
 import Navigation from "./navigation";
-import { Provider } from "react-redux";
-import { store,persistor } from "./src/Redux/store";
-import { PersistGate } from "redux-persist/integration/react";
+// import { Provider } from "react-redux";
+// import { store,persistor } from "./src/Redux/store";
+// import { PersistGate } from "redux-persist/integration/react";
 
 export default function App() {
     return (
-      <Provider store={store}>
-				<PersistGate loading={null} persistor={persistor}>
-                    <Navigation/>
-				</PersistGate>
-		  </Provider>
+      <Navigation/>
     );
 } 
